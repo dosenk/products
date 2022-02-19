ecdb5a99f18ffe8d786122d757eb715008811c36
Dmitry Osipenko <dmtr.osipenko@gmail.com>
2022-02-19 12:22:05 +0300

    feat: init store, BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Routes from './components/Routes';
+// import { useAppDispatch, useAppSelector } from './hooks/redux';
+// import { productApi, useFetchAppProductsQuery } from './services/ProducService';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <main>
+        <Routes />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
